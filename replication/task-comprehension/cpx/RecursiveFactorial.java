9a3ecea287e1582d7a0d5b930bf8e0168fa32aa0
Gilles Fabre <gillesfabre34@gmail.com>
2021-06-19 10:58:55 +0200

    Added java files for genese cpx

---
+++ replication/task-comprehension/cpx/RecursiveFactorial.java
@@ -0,0 +1,15 @@
+class GeneseCpx {
+
+    public static void main() {
+        int input = 4;
+        System.out.print(compute(input));
+    }
+
+    public static int compute(int value) {
+        if (value == 1) {
+            return 1;
+        }
+
+        return compute(value - 1) * value;
+    }
+}
